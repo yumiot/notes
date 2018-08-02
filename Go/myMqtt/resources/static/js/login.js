a157c5498956dc500a62b1b689c68a4eceac88a3
yumiot <yumiot@163.com>
2018-08-02 10:16:19 +0800

    rm mqtt

--- Go/myMqtt/resources/static/js/login.js
+++
@@ -1,8 +0,0 @@
-let modal = document.getElementById('id01');
-
-// 鼠标点击模型外区域关闭登录框
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
